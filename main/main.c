bbc110ff1a58eda9f0a1da0dda37f4c7f936b6d2
oyvindrankelarsen <larsen@larsen.tk>
2023-10-19 13:54:50 +0200

    first commit

---
+++ main/main.c
@@ -0,0 +1,272 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "esp_log.h"
+#include "connect_wifi.h"
+#include "esp_http_client.h"
+#include "esp_https_ota.h"
+#include "cJSON.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "esp_ota_ops.h"
+#include "esp_partition.h"
+
+// https://wokwi.com/projects/377761117596697601
+
+
+#define HASH_LEN 32
+#define FIRMWARE_VERSION	0.7
+#define UPDATE_JSON_URL		"https://github.com/oyvindrankelarsen/esp32ota/raw/main/bin/firmware.json"
+
+
+#define LED1_PIN 2
+#define LED2_PIN 4
+#define LED3_PIN 13
+
+#define THEPIN 2
+
+
+// receive buffer
+char rcv_buffer[200];
+
+// esp_http_client event handler
+esp_err_t _http_event_handler(esp_http_client_event_t *evt) {
+    
+	switch(evt->event_id) {
+        case HTTP_EVENT_REDIRECT:
+        case HTTP_EVENT_ERROR:
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            break;
+        case HTTP_EVENT_ON_DATA:
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+				strncpy(rcv_buffer, (char*)evt->data, evt->data_len);
+            }
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            break;
+    }
+    return ESP_OK;
+}
+
+
+
+
+void check_update_task(void *pvParameter) {
+	int cnt = 0;
+	while(1) {
+        char buf[255];
+        sprintf(buf, "%s?token=%d",UPDATE_JSON_URL,cnt);
+        cnt++;
+		printf("Looking for a new firmware at %s", buf);
+	
+		// configure the esp_http_client
+		esp_http_client_config_t config = {
+        .url = buf,
+        .event_handler = _http_event_handler,
+        .keep_alive_enable = true,        
+        .timeout_ms = 30000,
+		};
+		esp_http_client_handle_t client = esp_http_client_init(&config);
+	
+		// downloading the json file
+		esp_err_t err = esp_http_client_perform(client);
+		if(err == ESP_OK) {
+			
+			// parse the json file	
+			cJSON *json = cJSON_Parse(rcv_buffer);
+			if(json == NULL) printf("downloaded file is not a valid json, aborting...\n");
+			else {	
+				cJSON *version = cJSON_GetObjectItemCaseSensitive(json, "version");
+				cJSON *file = cJSON_GetObjectItemCaseSensitive(json, "file");
+				
+				// check the version
+				if(!cJSON_IsNumber(version)) printf("unable to read new version, aborting...\n");
+				else {
+					
+					double new_version = version->valuedouble;
+					if(new_version > FIRMWARE_VERSION) {
+						
+						printf("current firmware version (%.1f) is lower than the available one (%.1f), upgrading...\n", FIRMWARE_VERSION, new_version);
+						if(cJSON_IsString(file) && (file->valuestring != NULL)) {
+							printf("downloading and installing new firmware (%s)...\n", file->valuestring);
+							
+							esp_http_client_config_t ota_client_config = {
+								.url = file->valuestring,
+                                .keep_alive_enable = true,
+							};
+                            esp_https_ota_config_t ota_config = {
+                                    .http_config = &ota_client_config,
+                            };                            
+							esp_err_t ret = esp_https_ota(&ota_config);
+							if (ret == ESP_OK) {
+								printf("OTA OK, restarting...\n");
+								esp_restart();
+							} else {
+								printf("OTA failed...\n");
+							}
+						}
+						else printf("unable to read the new file name, aborting...\n");
+					}
+					else printf("current firmware version (%.1f) is greater or equal to the available one (%.1f), nothing to do...\n", FIRMWARE_VERSION, new_version);
+				}
+			}
+		}
+		else printf("unable to download the json file, aborting...\n");
+		
+		// cleanup
+		esp_http_client_cleanup(client);
+		
+		printf("\n");
+        vTaskDelay(30000 / portTICK_PERIOD_MS);
+    }
+}
+
+
+
+// void check_update_task(void *pvParameter) {
+	
+// 	while(1) {
+//         // char buf[255];
+//         // strcpy(buf,UPDATE_JSON_URL);
+//         // strcat();
+// 		printf("Looking for a new firmware...\n");
+	
+// 		// configure the esp_http_client
+// 		esp_http_client_config_t config = {
+//         .url = UPDATE_JSON_URL,
+//         .event_handler = _http_event_handler,
+// 		};
+// 		esp_http_client_handle_t client = esp_http_client_init(&config);
+	
+// 		// downloading the json file
+// 		esp_err_t err = esp_http_client_perform(client);
+// 		if(err == ESP_OK) {
+			
+// 			// parse the json file	
+// 			cJSON *json = cJSON_Parse(rcv_buffer);
+// 			if(json == NULL) printf("downloaded file is not a valid json, aborting...\n");
+// 			else {	
+// 				cJSON *version = cJSON_GetObjectItemCaseSensitive(json, "version");
+// 				cJSON *file = cJSON_GetObjectItemCaseSensitive(json, "file");
+				
+// 				// check the version
+// 				if(!cJSON_IsNumber(version)) printf("unable to read new version, aborting...\n");
+// 				else {
+					
+// 					double new_version = version->valuedouble;
+// 					if(new_version > FIRMWARE_VERSION) {
+						
+// 						printf("current firmware version (%.1f) is lower than the available one (%.1f), upgrading...\n", FIRMWARE_VERSION, new_version);
+// 						if(cJSON_IsString(file) && (file->valuestring != NULL)) {
+// 							printf("downloading and installing new firmware (%s)...\n", file->valuestring);
+							
+// 							esp_http_client_config_t ota_client_config = {
+// 								.url = file->valuestring,
+// 							};
+//                             esp_https_ota_config_t ota_config = {
+//                                     .http_config = &config,
+//                             };                            
+// 							esp_err_t ret = esp_https_ota(&ota_config);
+// 							if (ret == ESP_OK) {
+// 								printf("OTA OK, restarting...\n");
+// 								esp_restart();
+// 							} else {
+// 								printf("OTA failed...\n");
+// 							}
+// 						}
+// 						else printf("unable to read the new file name, aborting...\n");
+// 					}
+// 					else printf("current firmware version (%.1f) is greater or equal to the available one (%.1f), nothing to do...\n", FIRMWARE_VERSION, new_version);
+// 				}
+// 			}
+// 		}
+// 		else printf("unable to download the json file, aborting...\n");
+		
+// 		// cleanup
+// 		esp_http_client_cleanup(client);
+		
+// 		printf("\n");
+//         vTaskDelay(30000 / portTICK_PERIOD_MS);
+//     }
+// }
+
+static void print_sha256(const uint8_t *image_hash, const char *label)
+{
+    char hash_print[HASH_LEN * 2 + 1];
+    hash_print[HASH_LEN * 2] = 0;
+    for (int i = 0; i < HASH_LEN; ++i) {
+        sprintf(&hash_print[i * 2], "%02x", image_hash[i]);
+    }
+    ESP_LOGI("SHA", "%s %s", label, hash_print);
+}
+
+static void get_sha256_of_partitions(void)
+{
+    uint8_t sha_256[HASH_LEN] = { 0 };
+    esp_partition_t partition;
+
+    // get sha256 digest for bootloader
+    partition.address   = ESP_BOOTLOADER_OFFSET;
+    partition.size      = ESP_PARTITION_TABLE_OFFSET;
+    partition.type      = ESP_PARTITION_TYPE_APP;
+    esp_partition_get_sha256(&partition, sha_256);
+    print_sha256(sha_256, "SHA-256 for bootloader: ");
+
+    // get sha256 digest for running partition
+    esp_partition_get_sha256(esp_ota_get_running_partition(), sha_256);
+    print_sha256(sha_256, "SHA-256 for current firmware: ");
+}
+
+
+void app_main(void)
+{
+    gpio_reset_pin(LED1_PIN);
+    gpio_set_direction(LED1_PIN, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(LED2_PIN);
+    gpio_set_direction(LED2_PIN, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(LED3_PIN);
+    gpio_set_direction(LED3_PIN, GPIO_MODE_OUTPUT);    
+	esp_err_t ret = nvs_flash_init();
+    ESP_LOGI("CH", "%d ret", ret);
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+    get_sha256_of_partitions();
+
+	connect_wifi();
+
+    xTaskCreate(check_update_task, "check_update_task", 8192, NULL, 5, NULL);
+
+    while(1){
+        gpio_set_level(THEPIN,1 );
+        vTaskDelay(200 / portTICK_PERIOD_MS);        
+        gpio_set_level(LED1_PIN,0 );
+        vTaskDelay(200 / portTICK_PERIOD_MS);        
+    }
+}
